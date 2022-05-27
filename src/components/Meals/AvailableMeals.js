a30e3330f3a65680b6f5fcc29ab595a665a5bc04
Adityapk1234 <84319836+Adityapk1234@users.noreply.github.com>
2022-05-27 15:02:42 +0530

    Update AvailableMeals.js

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -11,7 +11,7 @@ const AvailableMeals = () => {
   useEffect(() => {
     const fetchMeals = async () => {
       const response = await fetch(
-        "https://react-http-ccb1c-default-rtdb.asia-southeast1.firebasedatabase.app/meals.json"
+        "https://food-9defe-default-rtdb.asia-southeast1.firebasedatabase.app/Meals.json"
       );
 
       if (!response.ok) {
